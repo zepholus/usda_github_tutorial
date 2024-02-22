49b3c98adc909cfd01d9274258841357bf99b052
jsalo <joansalograu@gmail.com>
2024-02-22 16:43:37 +0100

    module completed

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def groundwater_module(a, b):
+    return a + b
+
 def swat(a, b):
     print(a*b)
+
+
+def main():
+    a = 1
+    b = 2
+
+    return swat(a, b) + groundwater_module(a, b)
+
+main()
 
